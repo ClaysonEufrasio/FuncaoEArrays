40e144c9f3910415cfe881ecb596c9cfd8a4a244
ClaysonEufrasio <claysonrobertoeufrasio@gmail.com>
2022-04-11 19:24:50 -0300

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const participants = ["Rafael", "Manoel", "Daniel"]
+function positions(firstPlace, secondPlace, lastPlace) {
+
+    firstPlace = participants[0]
+    secondPlace = participants[2]
+    lastPlace = participants[1]
+    console.log(`
+The first runner is ${firstPlace}
+The second runner is ${secondPlace}
+The last runner is ${lastPlace}`)
+}
+
+positions()
