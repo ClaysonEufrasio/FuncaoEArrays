f46ed97c13287f391ca42d97e54d53f4b3e65737
ClaysonEufrasio <claysonrobertoeufrasio@gmail.com>
2022-04-11 21:38:44 -0300

    add

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const participants = ["Rafael", "Manoel", "Daniel"]
-function positions(firstPlace, secondPlace, lastPlace) {
-
-    firstPlace = participants[0]
-    secondPlace = participants[2]
-    lastPlace = participants[1]
-    console.log(`
-The first runner is ${firstPlace}
-The second runner is ${secondPlace}
-The last runner is ${lastPlace}`)
+function mudar(firstPlace, secondPlace, lastPlace) {
+    if (lastPlace === "camila") [firstPlace, secondPlace, lastPlace] = [firstPlace, lastPlace, secondPlace]
+    else if (secondPlace === "camila") [firstPlace, secondPlace, lastPlace] = [secondPlace, firstPlace, lastPlace]
+    else (firstPlace === "camila")[firstPlace, secondPlace, lastPlace] = [firstPlace, secondPlace, lastPlace]
+    return console.log(firstPlace, secondPlace, lastPlace)
 }
 
-positions()
+mudar("carlos", "jose", "camila")
+mudar("carlos", "camila", "jose")
+mudar("camila", "jose", "carlos")
